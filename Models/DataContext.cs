4d012aa912a49c3e0c92a48748c066d4f27ca3b4
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-07-26 17:40:00 +0300

    last change

---
+++ Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace User.Models
+{
+	public class DataContext: DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options):base(options)
+		{
+			Database.EnsureCreated();
+		}
+		public DbSet<SuperHero> SuperHeroes { get; set; } = null!;
+	}
+}
+
